752eae3affa6fe8186a9f09f89b4393ddf41d7c8
Vadim <108614816+Vadim-Savchuk@users.noreply.github.com>
2022-07-21 08:55:03 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// ========BURGER=============
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+// ==========SCROLL=============
+
+AOS.init();
